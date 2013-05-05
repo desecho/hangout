781000dc7299d8c840a4348511e6b9d34285ed1f
Desecho <desecho@gmail.com>
2013-05-05 10:02:55 +0400

    added new command to update visibility for new users

--- hangout_project/hangout/management/commands/new_user_registration_update_visibility.py
+++ hangout_project/hangout/management/commands/new_user_registration_update_visibility.py
@@ -1,12 +1,14 @@
 from django.core.management.base import BaseCommand
-from hangout.models import Visibility
+from hangout.models import Visibility, UserData
 
 
 class Command(BaseCommand):
-    help = 'Updates visibility'
+    help = 'Updates visibility when new user registers'
 
     def handle(self, *args, **options):
-        visibilities = Visibility.objects.all()
-        for visibility in visibilities:
-            visibility.visible = visibility.visible_updated
-            visibility.save()
+        users_data = UserData.objects.all()
+        for user_data in users_data:
+            friends_data = UserData.objects.exclude(user=user_data.user)
+            for friend_data in friends_data:
+                if not Visibility.objects.filter(user=user_data.user, friend=friend_data.user).exists():
+                    Visibility(user=user_data.user, friend=friend_data.user).save()
